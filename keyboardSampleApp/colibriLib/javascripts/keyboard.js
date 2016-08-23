9c09efb6648366206c031af84d0471358653cdfc
Gael Porte <gporte@adeneo-embedded.com>
2016-08-23 10:53:32 +0200

    App : Connect keyboard to text edit

---
+++ keyboardSampleApp/colibriLib/javascripts/keyboard.js
@@ -0,0 +1,168 @@
+/**
+ *  Copyright © 2010 Digia Plc
+ *  Copyright © 2010 Nokia Corporation
+ *
+ *  All rights reserved.
+ *
+ *  Nokia and Nokia Connecting People are registered trademarks of
+ *  Nokia Corporation.
+ *  Java and all Java-based marks are trademarks or registered
+ *  trademarks of
+ *  Sun Microsystems, Inc. Other product and company names
+ *  mentioned herein may be
+ *  trademarks or trade names of their respective owners.
+ *
+ *
+ *  Subject to the conditions below, you may, without charge:
+ *
+ *  ·  Use, copy, modify and/or merge copies of this software and
+ *     associated documentation files (the "Software"]
+ *
+ *  ·  Publish, distribute, sub-licence and/or sell new software
+ *     derived from or incorporating the Software.
+ *
+ *
+ *  This file, unmodified, shall be included with all copies or
+ *  substantial portions
+ *  of the Software that are distributed in source code form.
+ *
+ *  The Software cannot constitute the primary value of any new
+ *  software derived
+ *  from or incorporating the Software.
+ *
+ *  Any person dealing with the Software shall not misrepresent
+ *  the source of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY
+ *  KIND, EXPRESS OR IMPLIED,
+ *  INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ *  MERCHANTABILITY, FITNESS FOR A
+ *  PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT
+ *  HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ *  WHETHER IN AN ACTION
+ *  OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ *  CONNECTION WITH THE
+ *  SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+var row1Length = 10;
+var row2Length = 9;
+var row3Length = 11;
+
+/**
+ * Global object for keyboards abc buttons. It's possible to change
+ * Order of keyboards button from this file.
+ */
+var abc = [
+                //row1
+                ["q","Q"],
+                ["w","W"],
+                ["e","E"],
+                ["r","R"],
+                ["t","T"],
+                ["y","Y"],
+                ["u","U"],
+                ["i","I"],
+                ["o","O"],
+                ["p","P"],
+                //row2
+                ["a","A"],
+                ["s","S"],
+                ["d","D"],
+                ["f","F"],
+                ["g","G"],
+                ["h","H"],
+                ["j","J"],
+                ["k","K"],
+                ["l","L"],
+                //row3
+                ["z","Z"],
+                ["x","X"],
+                ["c","C"],
+                ["v","V"],
+                ["b","B"],
+                ["n","N"],
+                ["m","M"],
+                [",",";"],
+                [".",":"],
+                ["?","!"],
+                ["-","_"]
+                ];
+
+/**
+ * Global object for keyboards number buttons. It's possible to change
+ * Order of keyboards button from this file.
+ */
+var numbers = [
+                ["1",""],
+                ["2",""],
+                ["3",""],
+                ["4",""],
+                ["5",""],
+                ["6",""],
+                ["7",""],
+                ["8",""],
+                ["9",""],
+                ["0",""],
+                //row2
+                ["+",""],
+                ["-",""],
+                ["*",""],
+                ["/",""],
+                ["%",""],
+                ["=",""],
+                ["&",""],
+                ["@",""],
+                ["",""],
+                //row3
+                ["(","]"],
+                ["{","}"],
+                ["[","]"],
+                ["<",">"],
+                ["\u20AC",""],
+                ["$",""],
+                ["\u00A3",""],
+                ["~",""],
+                ["#",""],
+                ["^",""],
+                ["",""]
+            ];
+
+/**
+ * Global object for keyboards special characters. It's possible to change
+ * Order of keyboards button from this file.
+ */
+ var europeans = [
+                ["\u00E4"],
+                ["\u00F6"],
+                ["\u00E5"],
+                ["\u00E0"],
+                ["\u00E1"],
+                ["\u00E2"],
+                ["\u00E3"],
+                ["\u00E6"],
+                ["\u00E7"],
+                ["\u00E8"],
+                ["\u00E9"],
+                ["\u00EA"],
+                ["\u00EB"],
+                ["\u00EC"],
+                ["\u00ED"],
+                ["\u00EE"],
+                ["\u00EF"],
+                ["\u00F1"],
+                ["\u00F2"],
+                ["\u00F3"],
+                ["\u00F4"],
+                ["\u00F5"],
+                ["\u00F6"],
+                ["\u00F8"],
+                ["\u00F9"],
+                ["\u00FA"],
+                ["\u00FB"],
+                ["\u00FC"],
+                ["\u00FD"],
+                ["\u00FF"]
+            ];
+
+
--- keyboardSampleApp/main.qml
+++ keyboardSampleApp/main.qml
@@ -1,19 +1,43 @@
 import QtQuick 2.6
 import QtQuick.Window 2.2
 
+import "./colibriLib"
+
 Window {
     visible: true
-    width: 600
+    width: 1080
     height: 800
 
     title: qsTr("Colibri keyboard application")
 
-    TextEdit {
-        id : textEditItem
-        width : parent.width
-        height: parent.height / 2
-        anchors.top : parent.top
 
+    Rectangle{
+        anchors.fill : parent
         color : "grey"
+    }
+
+    Column {
+        anchors.fill : parent
+
+        Rectangle {
+            width : parent.width
+            height: parent.height / 2
+            border.width: 1
+            border.color: "#000000"
+            radius: 10
+
+            TextEdit {
+                anchors.fill: parent
+                anchors.margins : 10
+                text : globalKeyboard.text
+                font.pixelSize: 24
+            }
+        }
+
+        CLKeyboard{
+            id : globalKeyboard
+            width : parent.width
+            height: parent.height / 2
+        }
     }
 }
